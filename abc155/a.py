1debfb2687d093e11c4a52f3ddf5828f0ce2631f
Takaya <takaya1912@gmail.com>
2020-03-20 22:18:07 +0900

    success　A

--- abc155/a.py
+++ abc155/a.py
@@ -1,7 +1,17 @@
 import os
 
 def main():
-	pass	
+	num = list(map(int, input().split()))	
+	if num[0] == num[1] or num[0] == num[2]:
+		if num[1] == num[2]:
+			print("No")
+		else:
+			print("Yes")
+	else:
+		if num[1] == num[2]:
+			print("Yes")
+		else:
+			print("No")
 
 if __name__ == "__main__":
 	main()
