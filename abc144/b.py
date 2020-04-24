829f22c0fb770b340f2a17adf222916150512a7c
Takaya <takaya1912@gmail.com>
2020-04-25 00:50:43 +0900

    Atcoder abc144 b.py

---
+++ abc144/b.py
@@ -0,0 +1,16 @@
+import os
+
+def main():
+	n = int(input())
+	flag = False
+	for i in range(1,10):
+		if (n%i == 0) and (n//i < 10):
+			flag = True
+			break
+	if flag:
+		print("Yes")
+	else:
+		print("No")	
+
+if __name__ == "__main__":
+	main()
