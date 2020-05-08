d3519f23ce0d25b6ee1083c4f0fb2e33044f2d3a
Takaya <takaya1912@gmail.com>
2020-05-08 18:14:52 +0900

    abc140 c.cpp

--- abc140/c.cpp
+++ abc140/c.cpp
@@ -6,31 +6,26 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int b[n-1], result[n];
+	int b[n-1], a[n];
 	rep(i,n-1) cin >> b[i];
-	rep(i,n) {
-		result[i] = 0;
-	}
-	rep(i,n) {
-		if (i==0 or i==1) {
-			result[i] = b[0];
+	rep(i,n) a[i] = 0;
+	rep(i,2) a[i] = b[0];
+	//rep(i,n) cout << a[i] << endl;   
+
+	rep(i,n-1) {
+		//cout << i << endl;
+		if (b[i] >= max(a[i],a[i+1])) {
+			a[i+1] = b[i];
 		} else {
-			if (b[i] >= max(result[i],result[n-1])) {
-				//
-			} else {
-				result[i] = b[i];
-			}
-			if (i == n-1) {
-				result[i] = b[i-1];
-			}
-		}
+			a[i] = b[i];
+			a[i+1] = b[i];
+		} 
 	}
 	
 
 	int ans = 0;
 	rep(i,n) {
-		//cout << result[i] << endl;
-		ans += result[i];
+		ans += a[i];
 	}
 	cout << ans <<endl;
 	return 0;
