60915727f57bdc4157e5291a56b22526d700e348
Takaya <takaya1912@gmail.com>
2020-05-14 13:03:02 +0900

    abc145 a.cpp

---
+++ abc145/a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <iterator>
+#include <stack>
+#include <string.h>
+#include <cstdlib>
+#include <queue>
+#include <list>
+#include <string>
+#define rep(i,n) for(int i = 0; i < (n); i++) 
+using namespace std;
+
+int main() {
+	int r;
+	cin >> r;
+	cout << pow(r,2) << endl;
+	
+
+	return 0;
+}
