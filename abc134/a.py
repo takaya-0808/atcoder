1482ede798056990c2d5aa84bc442e5e5878cfd5
Takaya <takaya1912@gmail.com>
2020-05-30 22:05:47 +0900

    abc134 a.py

---
+++ abc134/a.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from collections import defaultdict, Counter
+from itertools import product, permutations,combinations, accumulate
+from operator import itemgetter
+from bisect import bisect_left,bisect
+from heapq import heappop,heappush
+from fractions import gcd
+from math import ceil, floor, sqrt
+from copy import deepcopy
+
+def main():
+    
+    r = int(input())
+    print(3*(r**2))
+
+if __name__ == "__main__":
+    main()
