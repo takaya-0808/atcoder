4120e90195efb9facad933ff58ed770aab2c21f1
Takaya <takaya1912@gmail.com>
2020-06-04 12:17:27 +0900

    atcoder 100

---
+++ abc100/a.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from collections import defaultdict, Counter
+from itertools import product, permutations,combinations, accumulate
+from operator import itemgetter
+from bisect import bisect_left,bisect
+from heapq import heappop,heappush,heapify
+from math import ceil, floor, sqrt, gcd
+from copy import deepcopy
+
+def main():
+    
+    a,b = map(int, input().split())
+    if a <= 8 and b <= 8:
+        print("Yay!")
+    else:
+        print(":(")
+    
+if __name__ == '__main__':
+    main()
