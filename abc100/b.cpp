8b0a0216e115862338bb12b7795f26264f4ea481
Takaya <takaya1912@gmail.com>
2020-06-17 13:47:51 +0900

    abc100 c++

---
+++ abc100/b.cpp
@@ -0,0 +1,37 @@
+#include<algorithm>//sort,二分探索,など
+#include<bitset>//固定長bit集合
+#include<cmath>//pow,logなど
+#include<complex>//複素数
+#include<deque>//両端アクセスのキュー
+#include<functional>//sortのgreater
+#include<iomanip>//setprecision(浮動小数点の出力の誤差)
+#include<iostream>//入出力
+#include<iterator>//集合演算(積集合,和集合,差集合など)
+#include<map>//map(辞書)
+#include<numeric>//iota(整数列の生成),gcdとlcm(c++17)
+#include<queue>//キュー
+#include<set>//集合
+#include<stack>//スタック
+#include<string>//文字列
+#include<unordered_map>//イテレータあるけど順序保持しないmap
+#include<unordered_set>//イテレータあるけど順序保持しないset
+#include<utility>//pair
+#include<vector>//可変長配列
+#define rep(i,n) for(int i = 0; i < (n); i++) 
+using namespace std;
+int long long ll;
+
+int main() {
+
+    int d,n;
+    cin >> d >> n;
+    
+    if (n==100) {
+        ll = (pow(100,d) * (n+1));
+        cout << ll << endl;
+    } else
+    {
+        cout << (pow(100, d) * n) << endl;
+    }
+    return 0;
+}
