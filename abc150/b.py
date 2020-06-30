48aab9760ea70011ffd51f3d71ceacaf4e31a188
Takaya <takaya1912@gmail.com>
2020-07-01 00:09:09 +0900

    abc150 b.py

--- abc150/b.py
+++ abc150/b.py
@@ -1,11 +1,20 @@
 import os
+import sys
+from collections import defaultdict, Counter
+from itertools import product, permutations,combinations, accumulate
+from operator import itemgetter
+from bisect import bisect_left,bisect
+from heapq import heappop,heappush,heapify
+from math import ceil, floor, sqrt
+from copy import deepcopy
+
 
 def main():
 	count = 0
 	num = int(input())
 	strings = input()
 	
-	for i in range(0,num-3,1):
+	for i in range(0,num-2,1):
 		if strings[i] == "A" and strings[i+1] == "B" and strings[i+2] == "C":	
 			count += 1
 	print(count)
