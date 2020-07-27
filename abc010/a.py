3b7c32a3d1f5900b5e55d445f6e9000bd7784e76
Takaya <takaya1912@gmail.com>
2020-07-27 12:09:44 +0900

    abc010 a.py

---
+++ abc010/a.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from collections import defaultdict, Counter
+from itertools import product, permutations,combinations, accumulate
+from operator import itemgetter
+from bisect import bisect_left,bisect
+from heapq import heappop,heappush,heapify
+from math import ceil, floor, sqrt
+from copy import deepcopy
+
+
+def main():
+    
+    s = input()
+    print(s + "pp")
+
+if __name__ == '__main__':
+	main()
